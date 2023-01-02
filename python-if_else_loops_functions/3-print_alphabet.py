c46672e1b4928ec50ad732d9086104647855a1b3
saijyothi3 <saijyothib@brightcomgroup.com>
2023-01-02 00:14:25 -0800

    hii

--- python-if_else_loops_functions/3-print_alphabet.py
+++ python-if_else_loops_functions/3-print_alphabet.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-for i in range(ord('a'), ord('z')+1):
-        if i == ord('q') or i == ord('e'):
-           continue
-        print("{:c}".format(i), end="") 
+for char in range(ord('a'), ord('z')+1):
+    if char == ord('q') or char == ord('e'):
+        continue
+    print("{:c}".format(char), end="")
