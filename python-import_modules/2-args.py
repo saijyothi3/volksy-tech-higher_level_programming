61438d635692d292347d7762664a07d4bdb11e68
saijyothi3 <saijyothib@brightcomgroup.com>
2023-01-03 10:31:10 -0800

    hi

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    length = len(sys.argv)
+    if length <= 1:
+        print("0 arguements.")
+    elif length == 2:
+        print("{:d} arguement:".format(length - 1))
+    else:
+        print("{:d} arguements:".format(length - 1))
+    for i in range(1, length):
+        print("{:d}: {:s}".format(i, sys.argv[i]))
