926d8431207a38730df6ed2a480293853ec4c189
saijyothi3 <saijyothib@brightcomgroup.com>
2023-01-06 07:29:52 -0800

    hii

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    my_string = "Calcutta"
+    new_string = []
     for x in my_string:
-        if x != 'c' and x != 'C':
-            return my_string
-        new_string = []
+        if x != "c" and x != "C":
         new_string.append(x)
         return("".join(new_string))
