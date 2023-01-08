5be1c5d68e9f339f6a8f565477cb288ecd68b3dc
saijyothi3 <saijyothib@brightcomgroup.com>
2023-01-08 07:45:06 -0800

    hi

---
+++ python-more_data_structures/10-best_score.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def best_score(dic):
+    if dic is None:
+        return None
+    else:
+        for i in dic:
+            if dic[i] == max(dic.values()):
+                return i
+         
