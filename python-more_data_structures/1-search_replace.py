e39f3ae393046adee99b31e85ea003c752d0ca44
saijyothi3 <saijyothib@brightcomgroup.com>
2023-01-08 03:58:06 -0800

    hi

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -3,5 +3,5 @@ def search_replace(my_list, search, replace):
     li = my_list[:]
     for i in range(len(li)):
         if li[i] == search:
-            li[i] == replace
-    return li   
+            li[i] = replace
+    return li  
