8d4b971baa13436892cacf337fe6f1f63790681a
saijyothi3 <saijyothib@brightcomgroup.com>
2023-01-11 09:55:46 -0800

    hi

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -1,15 +1,19 @@
 #!/usr/bin/python3
 """this is a class"""
 
+
 class Square:
     """this is bad"""
     def __init__(self, size=0):
         self.__size = size
+
     def area(self):
         return self.__size * self.__size
+
     @property
     def size(self):
         return self.__size
+
     @size.setter
     def size(self, value):
         if type(value) != int:
