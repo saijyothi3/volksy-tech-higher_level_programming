7728d68d66ceb9a304052b9f07e51392a6b297ed
saijyothi3 <saijyothib@brightcomgroup.com>
2023-01-11 10:34:11 -0800

    hi

---
+++ python-classes/5-square.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""square"""
+
+
+class Square:
+    """size of square"""
+    def __init__(self, size=0):
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+        @property
+        def size(self):
+            return self.__size
+
+        @size.setter
+        def size(self, value):
+            if type(value) is not int:
+                raise TypeError("size must be an integer")
+            if value < 0:
+                raise ValueError("size must be >= 0")
+
+                 self.__size = value
+
+        def area(self):
+            return self.__size * self.__size
+        def my_print(self):
+            if self.__size == 0:
+             print()
+             return
+        for i in range(0, self.__size):
+            for i in range(0, self.__size):
+                print(f"#", end='' if i < self.__size - 1 else '\n')
+
