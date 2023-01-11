93986fccdc83c830f8e5222a99c17b6fd2cb6277
saijyothi3 <saijyothib@brightcomgroup.com>
2023-01-11 11:40:19 -0800

    hi

--- python-classes/5-square.py
+++ python-classes/5-square.py
@@ -2,15 +2,11 @@
 """square"""
 
 
-class square:
+class Square:
     """size of square"""
 
     def __init__(self, size=0):
-        if type(size) is not int:
-            raise TypeError("size must be an integer")
-        if size < 0:
-            raise ValueError("size must be >= 0")
-        self.__size = size
+        self.size = size
 
         @property
         def size(self):
