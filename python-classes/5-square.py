b742fe4853bd687a7dd240e84d6efdf8b8ffbb4b
saijyothi3 <saijyothib@brightcomgroup.com>
2023-01-11 20:43:05 -0800

    hi

--- python-classes/5-square.py
+++ python-classes/5-square.py
@@ -8,12 +8,12 @@ class Square:
     def __init__(self, size=0):
         self.size = size
 
-        @property
+    @property
         def size(self):
             return self.__size
 
-        @size.setter
-        def size(self, value):
+    @size.setter
+    def size(self, value):
         if type(value) is int:
             if value >= 0:
                 self.__size = value
