75ee4c0fd14f86e339caa15e9272397dd4fce8ca
saijyothi3 <saijyothib@brightcomgroup.com>
2023-01-13 00:23:23 -0800

    hi

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""basegeometry"""
+
+
+class BaseGeometry():
+    """baseGeometry"""
+    def area(self):
+        """raise an exception"""
+        raise Exception("area() is not implemented")
