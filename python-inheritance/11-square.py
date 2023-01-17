a8753fd644d0b4c0bbf7ec2aa0c80d3691f423b7
saijyothi3 <saijyothib@brightcomgroup.com>
2023-01-16 22:13:38 -0800

    11-square.py

--- python-inheritance/11-square.py
+++ python-inheritance/11-square.py
@@ -1,22 +1,30 @@
 #!/usr/bin/python3
-"""module for Square."""
-
-
+"""This module contains a class Square that inherits from
+    Rectangle (9-rectangle.py). (task based on 10-square.py).
+"""
 Rectangle = __import__('9-rectangle').Rectangle
 
+
 class Square(Rectangle):
-    """class square"""
+    """class Square that inherits from Rectangle
+        (9-rectangle.py). (task based on 10-square.py).
+    Arguments:
+        Rectangle (cls) -- class
+    """
 
     def __init__(self, size):
-        """Initialize"""
-        self.integer_validator("size", size)
-        super().__init__(size, size)
+        """Instantiation with size.
+        Arguments:
+            size (int) -- size
+        """
+        super().integer_validator("size", size)
         self.__size = size
-
-    def area(self):
-        """Return the area"""
-        return self.__size ** 2
+        super().__init__(self.__size, self.__size)
 
     def __str__(self):
-        """Return string"""
-        return "[Square] " + str(self.__size) + "/" + str(self.__size)
+        """Returns the square description:
+            [Square] <width>/<height>
+        Returns:
+            str -- [Square] <width>/<height>
+        """
+        return "[Square] {}/{}".format(self.__size, self.__size)
