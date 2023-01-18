1705dc2d9f715d4d7a16c3f939cf8527db9807dd
saijyothi3 <saijyothib@brightcomgroup.com>
2023-01-18 03:45:34 -0800

    hi

--- python-input_output/4-from_json_string.py
+++ python-input_output/4-from_json_string.py
@@ -1,11 +1,15 @@
 #!/usr/bin/python3
-"""return object by a JSON"""
-
-
+""" Return an object by a JSON representation """
 import json
 
 
-def from_json-string(my_str):
-"""return object by JSON representation"""
+def from_json_string(my_str):
+    """
+    Return an object by a JSON representation.
+    Args:
+        my_str: JSON representation
+    Raises:
+        Exception: when the string can't be decoded
+    """
 
-     return json.loads(my_str)
+    return json.loads(my_str)
