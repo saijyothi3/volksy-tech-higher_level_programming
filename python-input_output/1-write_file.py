fc473e731694d35cca3b87d2a7d26d48f7e49ea0
saijyothi3 <saijyothib@brightcomgroup.com>
2023-01-18 03:08:34 -0800

    hi

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -1,8 +1,18 @@
 #!/usr/bin/python3
-"""writing string to a text file"""
+""" Module containing a function that writes to a file """
 
 
 def write_file(filename="", text=""):
-    """string to text file"""
-    with open(filename, mode="w", encoding="utf-8") as f:
-        return f.wite(text)
+    """
+    Function that writes to a text file.
+
+    Args:
+        filename: filename
+        text: text to write
+
+    Raises:
+        Exception: when the file can be opened
+    """
+
+    with open(filename, "w", encoding="utf-8") as f:
+        return f.write(text)
