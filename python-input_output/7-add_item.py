3461c3d7f2ef68660002879f8bb5dfe50cf07f0a
saijyothi3 <saijyothib@brightcomgroup.com>
2023-01-19 00:59:32 -0800

    hi

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,15 +1,18 @@
-#!/usr/bin/python3
-""" Add all arguments to a Python list and save them to a file """
+!/usr/bin/python3
+"""file added."""
+from pathlib import Path
 import sys
+import json
 
-if __name__ == "__main__":
-    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-    load_from_json_file = \
-        __import__('6-load_from_json_file').load_from_json_file
 
-    try:
-        items = load_from_json_file("add_item.json")
-    except FileNotFoundError:
-        items = []
-    items.extend(sys.argv[1:])
-    save_to_json_file(items, "add_item.json")
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
+args = []
+if Path('add_item.json').exists():
+    args = load_from_json_file('add_item.json')
+
+for i in range(1, len(sys.argv)):
+    args.append(str(sys.argv[i]))
+
+save_to_json_file(args, 'add_item.json')
