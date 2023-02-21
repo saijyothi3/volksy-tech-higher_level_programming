4fa19a17127586efef2718085bd8e2a2f60f1a0a
saijyothi3 <saijyothib@brightcomgroup.com>
2023-02-21 05:33:58 -0800

    hi

--- javascript-warm_up/7-multi_c.js
+++ javascript-warm_up/7-multi_c.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 const a = Number.parseInt(process.argv[2]);
-if (isNaN(a)){
-	console.log("Missing number of occurences");
-}else{
- for (let i=0;i<process.argv[2];i++){
-  console.log('C is fun');
- }
+if (isNaN(a)) {
+  console.log('Missing number of occurences');
+} else {
+  for (let i = 0; i < process.argv[2]; i++) {
+    console.log('C is fun');
+  }
 }
