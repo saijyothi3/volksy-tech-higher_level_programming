b9feb5f7a3ca0aad5017d259192dadd9107014ce
saijyothi3 <saijyothib@brightcomgroup.com>
2023-02-21 02:08:37 -0800

    hi

---
+++ javascript-warm_up/2-arguments.js
@@ -0,0 +1,13 @@
+#!/usr/binnode
+if (process.argv.length < 3)
+{
+	console.log('no argument');
+}
+else if (process.argv.length === 3)
+{
+	console.log('Argument found');
+}
+else
+{
+	console.log('Arguments found');
+}	
