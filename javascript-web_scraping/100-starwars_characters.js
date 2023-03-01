29cd5aa4c0b53bc66ead8727a298b7596ae77cba
saijyothi3 <saijyothib@brightcomgroup.com>
2023-03-01 04:25:13 -0800

    hi

--- javascript-web_scraping/100-starwars_characters.js
+++ javascript-web_scraping/100-starwars_characters.js
@@ -1,23 +1,15 @@
 #!/usr/bin/node
 const request = require('request');
-const url = 'http://swapi.co/api/films/' + process.argv[2];
-let movieDict = {};
-let characterDict = {};
-let i = 0;
+const url = 'https://swapi.co/api/films/' + process.argv[2];
 request(url, function (error, response, body) {
-  if (error) {
-    console.log(error);
-  } else {
-    movieDict = JSON.parse(body);
-    for (i = 0; i < movieDict.characters.length; i++) {
-      request(movieDict.characters[i], function (error, request, body) {
-        if (error) {
-          console.log(error);
-        } else {
-          characterDict = JSON.parse(body);
-          console.log(characterDict.name);
+  if (!error) {
+    const characters = JSON.parse(body).characters;
+    characters.forEach((character) => {
+      request(character, function (error, response, body) {
+        if (!error) {
+          console.log(JSON.parse(body).name);
         }
       });
-    }
+    });
   }
 });
