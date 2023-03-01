eb9c4c68da80fc64edfd9832078ec82b07c4115f
saijyothi3 <saijyothib@brightcomgroup.com>
2023-03-01 04:20:02 -0800

    hi

--- javascript-web_scraping/3-starwars_title.js
+++ javascript-web_scraping/3-starwars_title.js
@@ -8,21 +8,12 @@ const baseUrl = 'https://swapi-api.hbtn.io/api/films/';
 
 const filmId = process.argv[2];
 
-
-
-
 request.get(baseUrl + filmId, (err, response, body) => {
-
   if (err === null) {
-
     const data = JSON.parse(body);
 
     console.log(data.title);
-
   } else {
-
     console.log(err);
-
   }
-
 });
